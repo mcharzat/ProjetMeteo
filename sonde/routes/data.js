c7618b625a5acb6dede27c13907ca0ec86429967
Maxime_Charzat <23charzatcav@gmail.com>
2022-02-02 17:09:02 +0100

    Squelette sonde

---
+++ sonde/routes/data.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET  */
+router.get('/:parameterMeteo/:dateBegin,:dateEnd', function(req, res, next) {
+  
+});
+
+/* GET  */
+router.get('/:parameterMeteo/:dateBegin', function(req, res, next) {
+  
+});
+
+/* GET  */
+router.get('/:parameterMeteo', function(req, res, next) {
+  
+});
+
+module.exports = router;
