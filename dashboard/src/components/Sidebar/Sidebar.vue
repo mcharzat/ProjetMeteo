916953c5ccd4e119a6d8069586bb861a2bbfc710
Maxime_Charzat <23charzatcav@gmail.com>
2022-03-03 19:41:00 +0100

    change(dashboard): Change name of the window

--- dashboard/src/components/Sidebar/Sidebar.vue
+++ dashboard/src/components/Sidebar/Sidebar.vue
@@ -18,7 +18,7 @@
         class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
         to="/"
       >
-        Vue Notus
+        Weather Data
       </router-link>
       <!-- User -->
       <ul class="md:hidden items-center flex flex-wrap list-none">
